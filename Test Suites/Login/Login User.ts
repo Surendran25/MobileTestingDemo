bd10c77a4217ff52a0b87e67a33b4ae293247637
surendran.m <surendran.m@IVY-CRLSE-1034.ivygdcad.ivy-support.com>
2023-06-13 11:30:22 +0530

    2

---
+++ Test Suites/Login/Login User.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Login User</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>dd5e3867-0850-4c22-a4d0-7f1d76c498fd</testSuiteGuid>
+   <testCaseLink>
+      <guid>322bf73e-fa79-4fe7-a342-5d4c2913599d</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Login/User_Login</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
